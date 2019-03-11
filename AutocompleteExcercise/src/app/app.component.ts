f689dfb9fc5c0628b668a0f61b28916379683587
altair961 <altair961@users.noreply.github.com>
2019-03-11 18:19:42 +0300

    Created a project and installed ng-prime

---
+++ AutocompleteExcercise/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AutocompleteExcercise';
+}
